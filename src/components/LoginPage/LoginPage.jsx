f9ab44fa5f4fffcde6a6e7075842457f426e5b70
Akashajay-dot <akashajay339@gmail.com>
2024-05-09 03:48:15 +0530

    Layout

---
+++ src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,17 @@
+// import "../CSS/LoginPage.css"
+function LoginPage() {
+  return (
+    <div>
+      <div className="hero">
+        <h2>
+        What's your phone number or email?
+        </h2>
+        <input type="text" />
+        <button>Continue  </button>
+      </div>
+      
+    </div>
+  );
+}
+
+export default LoginPage;
